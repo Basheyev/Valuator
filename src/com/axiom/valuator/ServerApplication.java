1ec0227970e2376decec457445840617714640ef
Bolat Basheyev <b.basheyev@gmail.com>
2024-09-21 08:40:32 +0500

    refactored

--- src/com/axiom/valuator/ServerApplication.java
+++ src/com/axiom/valuator/ServerApplication.java
@@ -1,12 +1,8 @@
 package com.axiom.valuator;
 
 
-import com.axiom.valuator.services.RootService;
 import com.axiom.valuator.services.ValuationService;
-import org.eclipse.jetty.util.log.Log;
-import org.slf4j.simple.SimpleLoggerFactory;
 import spark.Spark;
-import spark.resource.ClassPathResourceHandler;
 
 public class ServerApplication {
 
@@ -14,9 +10,8 @@ public class ServerApplication {
     public static void main(String[] args) {
 
         Spark.port(80);
-        Spark.staticFileLocation("/public");
+        Spark.staticFileLocation("/main/resources/public");
         Spark.init();
-        Spark.get("/root", (req, res) -> new RootService());
         Spark.get("/valuate", new ValuationService());
         Spark.get("/shutdown", (request, response) -> { Spark.stop(); return "Server stopped"; } );
 
